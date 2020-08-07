e4be1584dd2d2dd44549a3fd3d07ddcafd36cc08
MihailKulikov <61084995+MihailKulikov@users.noreply.github.com>
2020-08-07 11:05:36 +0300

    CreateEmptyKernel (#40)
    * Add IOperationResult
    
    * Changed root namespace and added reference in all services

---
+++ DigitalOffice/Shared/Kernel/Broker/IOperationResult.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace LT.DigitalOffice.Kernel.Broker
+{
+    public interface IOperationResult<out T>
+    {
+        bool IsSuccess { get; }
+
+        List<string> Errors { get; }
+
+        T Body { get; }
+    }
+}
