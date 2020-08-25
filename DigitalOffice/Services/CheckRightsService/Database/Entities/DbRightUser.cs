486a2f13391ac27e5b8a63ca3e324828f35c5aae
ifelseelif <yougrysha@gmail.com>
2020-08-25 20:07:33 +0300

    added empty line

--- DigitalOffice/Services/CheckRightsService/Database/Entities/DbRightUser.cs
+++ DigitalOffice/Services/CheckRightsService/Database/Entities/DbRightUser.cs
@@ -10,6 +10,7 @@ namespace LT.DigitalOffice.CheckRightsService.Database.Entities
         public int RightId { get; set; }
         public DbRight Right { get; set; }
     }
+
     public class CompanyUserConfiguration : IEntityTypeConfiguration<DbRightUser>
     {
         public void Configure(EntityTypeBuilder<DbRightUser> builder)
